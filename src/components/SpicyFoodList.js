09301b434b48bbc3b878c565fea51c55afb50766
euigyu <euigyu.jung@gmail.com>
2022-04-24 19:32:52 -0700

    commit tree cfcccf0ad2820f5b0d7270e6cccf233e59702704

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,11 +6,27 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray)
     console.log(newFood);
   }
+function handleLiClick(id){
+  const newFoodArray =foods.map((food)=>{
+    if (food.id ===id){
+      return {
+        ...food,
+        heatLevel: food.heatLevel+1,
+      };
+    }else{
+        return food;
+      
+    }
+  });
+  setFoods(newFoodArray)
+}
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
@@ -22,5 +38,31 @@ function SpicyFoodList() {
     </div>
   );
 }
+const [filterBy, setFilterBy]=useState("All");
+
+function handleFilterChange(event){
+  setFilterBy(event.target.value);
+}
+return (
+  <select name="filter" onChange={handleFilterChange}>
+    <option value = "All">All</option>
+    <option value = "American">American</option>
+    <option value = "Sichuan">Sichuan</option>
+    <option value = "Thai">Thai</option>
+    <option value = "Mexican"></option>
+      </select>
+);
+const [foods, setFoods]= useState(spicyFoods);
+const [filterBy, setFilterBy] = useState("All");
+
+const FoodsToDisplay= food.filter((food) =>{
+  if (filterBy === "All"){
+    return true;
+  }else{
+    return food.cuisine === filterBy;
+  }
+});
+
+
 
 export default SpicyFoodList;
